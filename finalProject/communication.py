1bfb58c55591abb62f767bb0e53c293dadf95ab9
Sagnik <48783211+iSagnik@users.noreply.github.com>
2022-04-28 10:52:01 -0400

    Update communication.py

--- finalProject/communication.py
+++ finalProject/communication.py
@@ -3,9 +3,9 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "AC830f3ae839a9e467da9c46c2c770a7b8"
-auth_token = "0588b5bd57388181f94a8cc7437ff4bd"
-from_user = '+19205888804'
+account_sid = ""
+auth_token = ""
+from_user = ""
 client = Client(account_sid, auth_token)
 
 def send_message(number, msg):
